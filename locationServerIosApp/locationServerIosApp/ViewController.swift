11a913e5449221c22571fd8fcd278b75120dacf6
stayzilla <asif.junaid@stayzilla.com>
2015-12-08 16:38:24 +0530

    adding initial setup ..

---
+++ locationServerIosApp/locationServerIosApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  locationServerIosApp
+//
+//  Created by Asif Junaid on 12/8/15.
+//  Copyright © 2015 Asif Junaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
